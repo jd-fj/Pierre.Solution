35a78d2bdfdee4c2c63f018cbb563172d9bc74dd
taylulz <taylulzcode@gmail.com>
2020-12-11 10:56:09 -0800

    purposefully fail test

--- Pierre.Tests/ModelTests/BreadTests.cs
+++ Pierre.Tests/ModelTests/BreadTests.cs
@@ -9,8 +9,18 @@ namespace Pierre.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
-      Assert.AreEqual(typeof(Bread), newBread.GetType());
+      Bread newBreadOrder = new Bread(2);
+      Assert.AreEqual(typeof(Bread), newBreadOrder.GetType());
+    }
+    [TestMethod]
+    public void
+    GetTotalCost_ReturnsTotalCostOfBread_Int()
+    {
+      int quantity = 2;
+      int falseQuant = 3;
+      Bread newBreadOrder = new Bread(quantity);
+      int result = newBreadOrder.Quantity;
+      Assert.AreEqual(falseQuant, result);
     }
   }
 }
