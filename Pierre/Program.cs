9534223523d8c34b0d9ea5d21a97463da3962caf
taylulz <taylulzcode@gmail.com>
2020-12-13 14:11:16 -0800

    outline steps to build UI

--- Pierre/Program.cs
+++ Pierre/Program.cs
@@ -1,12 +1,35 @@
 using System;
 using Pierre.Models;
+using System.Collections.Generic;
 
 class Program
 {
-  static void Main()
+  public static Bread pita = new Bread("pita", 0);
+  public static Bread french = new Bread("french", 0);
+
+  public static List<Bread> allBread = new List<Bread>() {pita, french};
+
+  public static Pastry biscuit = new Pastry("biscuit", 0);
+  public static Pastry croissant = new Pastry("croissant", 0);
+
+  public static List<Pastry> allPastries = new List<Pastry>() {biscuit, croissant};
+
+
+  public static void Main()
   {
-    // Console.WriteLine("Hello ");
-    // int quantity = int.Parse(Console.ReadLine());
-    // Console.WriteLine(quantity);
+    Console.WriteLine("Hello ");
+
+    foreach (Bread loaf in allBread)
+    {
+    Console.WriteLine(allBread);
+    }
   }
 } 
+
+// I want the console to do things in this order:
+// 1. Start screen lists breads and pastries
+// 2. Ask what type and quantity of bread
+// 3. Ask what type and quantity of pastry
+// 4. Ask if they need to add anything else (if so run through main again)
+// 5. Once the answer is "no" to needing anything else...
+// 6. Print the grand total and what they ordered
