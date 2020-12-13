fa9ab72b4b12466e17eaf25400d24eb16e2569b1
taylulz <taylulzcode@gmail.com>
2020-12-13 11:40:07 -0800

    add static list for all Bread objects to test with

--- Pierre/Models/Bread.cs
+++ Pierre/Models/Bread.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Pierre.Models
 {
@@ -8,6 +9,7 @@ namespace Pierre.Models
     public int Quantity { get; }
     public int TotalCost { get; }
     public double SaleCost { get; }
+    private static List<Bread> _breadOrder = new List<Bread> {};
 
     public Bread(string breadKind, int quantity)
     {
@@ -15,7 +17,13 @@ namespace Pierre.Models
       Quantity = quantity;
       TotalCost = quantity * 5;
       SaleCost = Math.Round(quantity * (3.33));
+      _breadOrder.Add(this);
     }
-    
+
+    public static List<Bread> GetAll()
+    {
+      return _breadOrder;
+    }
+
   }
 }
