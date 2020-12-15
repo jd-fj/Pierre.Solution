5a8bd0062604dc53a1b5483c5df5ac8e072ba5f9
taylulz <taylulzcode@gmail.com>
2020-12-14 20:07:21 -0800

    dry up code remove non-working comments

--- Pierre/Program.cs
+++ Pierre/Program.cs
@@ -8,12 +8,9 @@ namespace Pierre
   {
     public static string[] breadMenu = { "Pita", "French" };
     public static string[] pastryMenu = { "Biscuits", "Croissants" };
-    // private static List<Bread> breadCart = new List<Bread> {};
-    // private static List<Pastry> pastryCart = new List<Pastry> {};
-    
+
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("Would you like to order some of our fine Breads? ['Y' for yes, 'N' to check out the pastries]");
       string breadYesOrNo = Console.ReadLine().ToLower();
       if (breadYesOrNo == "y")
@@ -28,20 +25,20 @@ namespace Pierre
 
     public static void OrderBread()
     {
-      Console.WriteLine("Please select from our following bread items:");
       Console.WriteLine("--------Bread Menu:--------");
       foreach (string loaf in breadMenu)
       {
       Console.WriteLine("Pierre's: " + loaf + " bread");
       }
-      Console.WriteLine("----------------");
-      Console.WriteLine("What type of bread do you want? ");
+      Console.WriteLine();
+      Console.WriteLine("What type of bread do you want?");
       string breadSelection = Console.ReadLine();
       Console.WriteLine("Va bene! Pierre's " + breadSelection + " bread is the finest");
       Console.WriteLine("How many of these sweet baby " + breadSelection + " loaves do you want?");
       int breadQuant = int.Parse(Console.ReadLine());
       Bread breadOrder = new Bread(breadSelection, breadQuant);
       Console.WriteLine("you ordered " + breadOrder.Quantity + " of the " + breadOrder.BreadKind);
+      // potentially loop back to top of OrderBread() here?
       Console.WriteLine("Total Bread Cost: " + breadOrder.TotalCost);
       Console.WriteLine("Sale Price: " + breadOrder.SaleCost);
       Console.WriteLine("Total Savings: " + (breadOrder.TotalCost - breadOrder.SaleCost));
@@ -52,24 +49,26 @@ namespace Pierre
 
     public static void OrderPastry()
     {
-      Console.WriteLine("What type of pastry would you like? [enter 'N' to checkout]" );
+      Console.WriteLine("Would you like to order a pastry? ['Y' for yes, 'N' to checkout]");
       string pastryYesOrNo = Console.ReadLine().ToLower();
       if (pastryYesOrNo == "n")
       {
-        // checkout 
+        CheckOut(); 
       }
-      else
+      if (pastryYesOrNo == "y")
       {
-      Console.WriteLine("--------Pastry Menu--------");
+      Console.WriteLine("--------:Pastry Menu:--------");
       foreach (string pastry in pastryMenu)
       {
         Console.WriteLine("Pierre's: " + pastry + " pastry");
       }
-      Console.WriteLine("----------------");
+      Console.WriteLine();
+      Console.WriteLine("What type of pastry do you want?");
       string pastrySelection = Console.ReadLine();
       Console.WriteLine("How many of these little " + pastrySelection + " do you want?");
       int pastryQuant = int.Parse(Console.ReadLine());
       Pastry newPastryOrder = new Pastry(pastrySelection, pastryQuant);
+      // potentially add loop to go back to OrderPastry for more?
       Console.WriteLine("Total Pastry Cost: " + newPastryOrder.TotalCost);
       Console.WriteLine("Sale Price: " + newPastryOrder.SaleCost);
       Console.WriteLine("Total Savings: " + (newPastryOrder.TotalCost - newPastryOrder.SaleCost));
@@ -79,7 +78,6 @@ namespace Pierre
 
     public static void CheckOut()
     {
-      // Console.WriteLine("BreadCart " + breadCart );
       Console.WriteLine("Would you like to order more? ['Y' for yes, 'N' for no]");
       string goodBye = Console.ReadLine().ToLower();
       
@@ -92,8 +90,8 @@ namespace Pierre
         List<Bread> breadBasket = Bread.GetAll();
         foreach (Bread loaf in breadBasket)
         {
-          double grandTotal =+ loaf.SaleCost;
-          Console.WriteLine("Your Bread Total is: $" + grandTotal);
+          double breadTotal =+ loaf.SaleCost;
+          Console.WriteLine("Your Bread Total is: $" + breadTotal);
         }
       }
       else
@@ -103,12 +101,4 @@ namespace Pierre
 
     }
   }
-} 
-
-// I want the console to do things in this order:
-// 1. Start screen lists breads and pastries
-// 2. Ask what type and quantity of bread
-// 3. Ask what type and quantity of pastry
-// 4. Ask if they need to add anything else (if so run through main again)
-// 5. Once the answer is "no" to needing anything else...
-// 6. Print the grand total and what they ordered
+}
