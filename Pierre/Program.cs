fdc441848fb3fa02fcdaa2ceae74e5957ce93a19
taylulz <taylulzcode@gmail.com>
2020-12-22 06:02:58 -0800

    Working bread order UI section

--- Pierre/Program.cs
+++ Pierre/Program.cs
@@ -15,24 +15,31 @@ namespace Pierre
       string breadYesOrNo = Console.ReadLine().ToLower();
       if (breadYesOrNo == "y")
       {
-        Console.WriteLine("What type of bread do you want?['french' or 'pita']);
-        
+        Console.WriteLine("What type of bread do you want?['french' or 'pita']");
+        string breadSelection = Console.ReadLine();
+        Console.WriteLine("How many?");
+        int breadQuant = int.Parse(Console.ReadLine());
+        Bread breadOrder = new Bread(breadSelection, breadQuant);
+        int breadGt = breadOrder.GetSaleCost();
+        Console.WriteLine("Your Sale Price: " + breadGt);
+        Main();
       }
+
       else
       {
-        Console.WriteLine("Would you like to order some of our fine Pastries? ['Y' for yes, 'Enter' for no]")
+        Console.WriteLine("Would you like to order some of our fine Pastries? ['Y' for yes, 'Enter' for no]");
         string pastryYesOrNo = Console.ReadLine().ToLower();
         if (pastryYesOrNo == "y")
         {
-          do pastry order
+          // return pastryOrder;
         }
         else
         {
-          Console.WriteLine("Press enter to check out")
+          Console.WriteLine("Press enter to check out");
         }
       }
-      Console.WriteLine(BreadTotal)
-      Console.WriteLine(PastryTotal)
+      // Console.WriteLine(BreadTotal);
+      // Console.WriteLine(PastryTotal);
     }
 
   }
