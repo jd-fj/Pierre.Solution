64a95c531df15ee723bc3d61bca4a0499856d2db
taylulz <taylulzcode@gmail.com>
2020-12-22 06:54:39 -0800

    working bread/pastry Grand Totals

--- Pierre/Program.cs
+++ Pierre/Program.cs
@@ -13,6 +13,7 @@ namespace Pierre
     {
       Console.WriteLine("Would you like to order some of our fine Breads? ['Y' for yes, 'Enter' for no]");
       string breadYesOrNo = Console.ReadLine().ToLower();
+      
       if (breadYesOrNo == "y")
       {
         Console.WriteLine("What type of bread do you want?['french' or 'pita']");
@@ -21,40 +22,34 @@ namespace Pierre
         int breadQuant = int.Parse(Console.ReadLine());
         Bread breadOrder = new Bread(breadSelection, breadQuant);
         int breadGt = breadOrder.GetSaleCost();
-        Console.WriteLine("Your Sale Price: " + breadGt);
         Main();
       }
 
-      else
+      Console.WriteLine("Would you like to order some of our fine Pastries? ['Y' for yes, 'Enter' for no]");
+      string pastryYesOrNo = Console.ReadLine().ToLower();
+      if (pastryYesOrNo == "y")
       {
-        Console.WriteLine("Would you like to order some of our fine Pastries? ['Y' for yes, 'Enter' for no]");
-        string pastryYesOrNo = Console.ReadLine().ToLower();
-        if (pastryYesOrNo == "y")
-        {
-          Console.WriteLine("What type of pastry do you want?");
-          string pastrySelection = Console.ReadLine();
-          Console.WriteLine("How many?");
-          int pastryQuant = int.Parse(Console.ReadLine());
-          Pastry newPastryOrder = new Pastry(pastrySelection, pastryQuant);
-          int pastryGt = newPastryOrder.GetSaleCost();
-          Console.WriteLine("Sale Price: " + pastryGt);
-        }
-        else
-        {
-          List<Bread> breadBasket = Bread.GetAll();
-          List<Pastry> pastryBasket = Pastry.GetAll();
-          foreach (Bread bread in breadBasket)
-          {
-            int breadGt = bread.GetSaleCost();
-            Console.WriteLine("Bread: " + bread.GetSaleCost());
-          }
-
-        }
-        
+        Console.WriteLine("What type of pastry do you want?");
+        string pastrySelection = Console.ReadLine();
+        Console.WriteLine("How many?");
+        int pastryQuant = int.Parse(Console.ReadLine());
+        Pastry newPastryOrder = new Pastry(pastrySelection, pastryQuant);
+        int pastryGt = newPastryOrder.GetSaleCost();
       }
-      // Console.WriteLine(BreadTotal);
-      // Console.WriteLine(PastryTotal);
-    }
 
+      List<Bread> breadBasket = Bread.GetAll();
+      List<Pastry> pastryBasket = Pastry.GetAll();
+      foreach (Bread bread in breadBasket)
+      {
+        int breadGt = bread.GetSaleCost();
+        Console.WriteLine("Total Bread: " + breadGt);
+      }
+
+      foreach (Pastry pastry in pastryBasket)
+      {
+        int pastryGt = pastry.GetSaleCost();
+        Console.WriteLine("Total Pastry: " + pastryGt);
+      }
+    }
   }
 }
